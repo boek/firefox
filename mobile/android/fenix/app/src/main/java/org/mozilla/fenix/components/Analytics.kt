081551902339c3ccdc11d1d5e0ae464381000931
Jeff Boek <j@jboek.com>
2025-09-16 06:48:45 -0700

    Bug 1987536 - Updates Fenix to support new crash metadata
    - Use new socorro service
    - Pass event processor to the Sentry Service
    - Adds new runtime tag providers to the crash reporter
    
    Differential Revision: https://phabricator.services.mozilla.com/D264948

--- mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Analytics.kt
+++ mobile/android/fenix/app/src/main/java/org/mozilla/fenix/components/Analytics.kt
@@ -8,16 +8,23 @@ import android.app.Application
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageInfo
+import android.content.pm.PackageManager
 import android.os.Build
 import mozilla.components.lib.crash.CrashReporter
+import mozilla.components.lib.crash.runtimetagproviders.BuildRuntimeTagProvider
+import mozilla.components.lib.crash.runtimetagproviders.EnvironmentRuntimeProvider
+import mozilla.components.lib.crash.runtimetagproviders.VersionInfoProvider
 import mozilla.components.lib.crash.sentry.SentryService
+import mozilla.components.lib.crash.sentry.eventprocessors.CrashMetadataEventProcessor
 import mozilla.components.lib.crash.service.CrashReporterService
 import mozilla.components.lib.crash.service.GleanCrashReporterService
-import mozilla.components.lib.crash.service.MozillaSocorroService
+import mozilla.components.lib.crash.service.socorro.MozillaSocorroService
 import mozilla.components.lib.crash.store.CrashReportOption
 import mozilla.components.support.ktx.android.content.isMainProcess
 import mozilla.components.support.utils.BrowsersCache
 import mozilla.components.support.utils.RunWhenReadyQueue
+import mozilla.components.support.utils.ext.getPackageInfoCompat
 import org.mozilla.fenix.BuildConfig
 import org.mozilla.fenix.Config
 import org.mozilla.fenix.HomeActivity
@@ -73,6 +80,7 @@ class Analytics(
                 sendEventForNativeCrashes = false, // Do not send native crashes to Sentry
                 sendCaughtExceptions = shouldSendCaughtExceptions,
                 sentryProjectUrl = getSentryProjectUrl(),
+                crashMetadataEventProcessor = CrashMetadataEventProcessor(),
             )
 
             // We only want to initialize Sentry on startup on the main process.
@@ -90,8 +98,6 @@ class Analytics(
         val socorroService = MozillaSocorroService(
             context,
             appName = "Fenix",
-            version = MOZ_APP_VERSION,
-            buildId = MOZ_APP_BUILDID,
             vendor = MOZ_APP_VENDOR,
             releaseChannel = MOZ_UPDATE_CHANNEL,
             distributionId = distributionId,
@@ -136,6 +142,8 @@ class Analytics(
                 !context.settings().useNewCrashReporterDialog,
             runtimeTagProviders = listOf(
                 ReleaseRuntimeTagProvider(),
+                BuildRuntimeTagProvider(context.versionInfoProvider),
+                EnvironmentRuntimeProvider(),
                 NimbusExperimentsRuntimeTagProvider(
                     nimbusApi = lazyMonitored { nimbusComponents.sdk },
                 ),
@@ -188,3 +196,9 @@ private fun getSentryProjectUrl(): String? {
         else -> null
     }
 }
+
+private val Context.versionInfoProvider: VersionInfoProvider
+    get() {
+        val packageInfo = applicationContext.packageManager.getPackageInfoCompat(applicationContext.packageName, 0)
+        return VersionInfoProvider.fromPackageInfo(packageInfo)
+    }
